c0984e6bd6af62c01fdd9497fad65c3226e4dacb
snuegg <evr456@snu.ac.kr>
2015-12-30 03:24:04 +0000

    gg

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
     def home
-        
+    end
+    def onehof
+    
+    end
+    def jujum
+    end
+    def dong
+    end
+    def festival
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/home' => 'home#home' 
+  get 'home/home' => 'home#home'
+  root 'home#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
